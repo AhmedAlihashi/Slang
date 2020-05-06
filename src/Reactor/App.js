03bdebf622046d428e80a5a51043b505e687ad27
Ahmed <ahmedali.96@hotmail.com>
2020-05-06 02:18:20 -0400

    app header & container stabilized

--- src/Reactor/App.js
+++ src/Reactor/App.js
@@ -18,189 +18,203 @@ const App = () => {
   useEffect(() => {}, []);
   console.log("width - height ", window.innerWidth, window.innerHeight);
   return (
-    <Sidebar.Pushable as="div">
-      <Sidebar
-        as={Menu}
-        animation="slide along"
-        icon="labeled"
-        inverted
-        onHide={() => setSidebarVisible(!sidebarVisible)}
-        vertical
-        visible={sidebarVisible}
-        width="thin"
-      >
-        <Menu.Item as="a">Home</Menu.Item>
-        <Menu.Item as="a">Games</Menu.Item>
-        <Menu.Item as="a">Channels</Menu.Item>
-      </Sidebar>
-      <Sidebar.Pusher dimmed={sidebarVisible}>
-        <Segment basic style={{ padding: 0 }}>
-          <div className="AppHeader">
-            <div style={{ margin: 10 }}>
-              <Button
-                fluid
-                compact
-                id="NoDrag"
-                icon="bars"
-                color="black"
-                style={{ fontSize: "1.5vw" }}
-                onClick={() => {
-                  setSidebarVisible(!sidebarVisible);
-                }}
-              />
+    <Segment inverted tertiary style={{ padding: 0 }}>
+      <Sidebar.Pushable as="div">
+        <Sidebar
+          as={Menu}
+          animation="slide along"
+          icon="labeled"
+          inverted
+          onHide={() => setSidebarVisible(!sidebarVisible)}
+          vertical
+          visible={sidebarVisible}
+          width="thin"
+        >
+          <Menu.Item as="a">Home</Menu.Item>
+          <Menu.Item as="a">Games</Menu.Item>
+          <Menu.Item as="a">Channels</Menu.Item>
+        </Sidebar>
+        <Sidebar.Pusher dimmed={sidebarVisible}>
+          <Segment basic style={{ padding: 0 }}>
+            <div className="AppHeader">
+              <Grid celled="internally">
+                <Grid.Row>
+                  <Grid.Column width={4}>
+                    <Header as="h1" style={{ color: "white" }}>
+                      <Button
+                        compact
+                        id="NoDrag"
+                        icon="bars"
+                        color="black"
+                        style={{ fontSize: "1w" }}
+                        onClick={() => {
+                          setSidebarVisible(!sidebarVisible);
+                        }}
+                      />
+                      Slang.
+                    </Header>
+                  </Grid.Column>
+                  <Grid.Column width={4}>
+                    <Header as="h3" style={{ color: "white" }}>
+                      <Header.Content>
+                        Account Settings
+                        <Header.Subheader>
+                          Manage your preferences
+                        </Header.Subheader>
+                      </Header.Content>
+                    </Header>
+                  </Grid.Column>
+
+                  <Grid.Column width={5} />
+
+                  <Grid.Column width={3}>
+                    <Button.Group id="NoDrag" floated="right">
+                      <Button icon>
+                        <Icon className="bell outline icon" />
+                      </Button>
+                      <Button content="Logout" />
+                      <Button
+                        icon
+                        color="red"
+                        onClick={() => {
+                          window.close();
+                        }}
+                      >
+                        <Icon className="power off icon" />
+                      </Button>
+                    </Button.Group>
+                  </Grid.Column>
+                </Grid.Row>
+              </Grid>
             </div>
 
-            <div className="AppTitle">
-              <div>
-                <Header as="h1" style={{ color: "white" }}>
-                  Slang.
-                </Header>
-              </div>
-              <Header as="h2" floated="right" id="NoDrag">
-                <Button.Group>
-                  <Button icon>
-                    <Icon className="bell outline icon" />
-                  </Button>
-                  <Button content="Logout" />
-                  <Button
-                    icon
-                    color="red"
-                    onClick={() => {
-                      window.close();
-                    }}
-                  >
-                    <Icon className="power off icon" />
-                  </Button>
-                </Button.Group>
-              </Header>
-            </div>
-          </div>
-          <Grid columns={2}>
-            <Grid.Row stretched>
-              <Grid.Column width={2}>
-                <Segment
-                  vertical
-                  id="NoDrag"
-                  inverted
-                  style={{
-                    height: "100vh",
-                    alignItems: "flex-start",
-                    justifyItems: "center",
-                    backgroundColor: "#2d5857",
-                  }}
-                >
+            <Grid columns={2}>
+              <Grid.Row stretched>
+                <Grid.Column width={2}>
                   <Segment
                     vertical
                     id="NoDrag"
                     inverted
                     style={{
-                      display: "flex",
+                      height: "100vh",
                       alignItems: "flex-start",
-                      justifyContent: "center",
-                      marginTop: -15,
-                      marginBottom: 15,
-                      border: "solid 5px black",
-                      backgroundColor: "#7C8C8C",
+                      justifyItems: "center",
+                      backgroundColor: "#2d5857",
                     }}
                   >
-                    <Button.Group vertical>
-                      <Button
-                        icon="expand"
-                        color="black"
-                        style={{ fontSize: "2.5vw" }}
-                        onClick={() => {
-                          console.log("Picture-in-picture");
-                        }}
-                      />
-                      <Button
-                        icon="expand arrows alternate"
-                        color="black"
-                        style={{ fontSize: "2.5vw" }}
-                        onClick={() => {
-                          console.log("Fullscreen video");
-                        }}
-                      />
-                      <Button
-                        icon="shuffle"
-                        color="black"
-                        style={{ fontSize: "2.5vw" }}
-                        onClick={() => {
-                          console.log("Random video");
-                        }}
-                      />
-                    </Button.Group>
+                    <Segment
+                      vertical
+                      id="NoDrag"
+                      inverted
+                      style={{
+                        display: "flex",
+                        alignItems: "flex-start",
+                        justifyContent: "center",
+                        marginTop: -15,
+                        marginBottom: 15,
+                        border: "solid 5px black",
+                        backgroundColor: "#7C8C8C",
+                      }}
+                    >
+                      <Button.Group vertical>
+                        <Button
+                          icon="expand"
+                          color="black"
+                          style={{ fontSize: "2.5vw" }}
+                          onClick={() => {
+                            console.log("Picture-in-picture");
+                          }}
+                        />
+                        <Button
+                          icon="expand arrows alternate"
+                          color="black"
+                          style={{ fontSize: "2.5vw" }}
+                          onClick={() => {
+                            console.log("Fullscreen video");
+                          }}
+                        />
+                        <Button
+                          icon="shuffle"
+                          color="black"
+                          style={{ fontSize: "2.5vw" }}
+                          onClick={() => {
+                            console.log("Random video");
+                          }}
+                        />
+                      </Button.Group>
+                    </Segment>
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "flex-start",
+                        justifyContent: "center",
+                        width: "100%",
+                      }}
+                    >
+                      <Button.Group vertical>
+                        <Button
+                          style={{ fontSize: "2.5vw" }}
+                          icon="user circle"
+                          color="black"
+                          onClick={() => {
+                            console.log("Picture-in-picture");
+                          }}
+                        />
+                        <Button
+                          style={{ fontSize: "2.5vw" }}
+                          icon="search"
+                          color="black"
+                          onClick={() => {
+                            console.log("Fullscreen video");
+                          }}
+                        />
+                        <Button
+                          style={{ fontSize: "2.5vw" }}
+                          icon="hotjar"
+                          color="black"
+                          onClick={() => {
+                            console.log("Random video");
+                          }}
+                        />
+                        <Button
+                          style={{ fontSize: "2.5vw" }}
+                          icon="expand"
+                          color="black"
+                          onClick={() => {
+                            console.log("Picture-in-picture");
+                          }}
+                        />
+                        <Button
+                          style={{ fontSize: "2.5vw" }}
+                          icon="expand arrows alternate"
+                          color="black"
+                          onClick={() => {
+                            console.log("Fullscreen video");
+                          }}
+                        />
+                        <Button
+                          style={{ fontSize: "2.5vw" }}
+                          icon="history"
+                          color="black"
+                          onClick={() => {
+                            console.log("Random video");
+                          }}
+                        />
+                      </Button.Group>
+                    </div>
                   </Segment>
-                  <div
-                    style={{
-                      display: "flex",
-                      alignItems: "flex-start",
-                      justifyContent: "center",
-                      width: "100%",
-                    }}
-                  >
-                    <Button.Group vertical>
-                      <Button
-                        style={{ fontSize: "2.5vw" }}
-                        icon="user circle"
-                        color="black"
-                        onClick={() => {
-                          console.log("Picture-in-picture");
-                        }}
-                      />
-                      <Button
-                        style={{ fontSize: "2.5vw" }}
-                        icon="search"
-                        color="black"
-                        onClick={() => {
-                          console.log("Fullscreen video");
-                        }}
-                      />
-                      <Button
-                        style={{ fontSize: "2.5vw" }}
-                        icon="hotjar"
-                        color="black"
-                        onClick={() => {
-                          console.log("Random video");
-                        }}
-                      />
-                      <Button
-                        style={{ fontSize: "2.5vw" }}
-                        icon="expand"
-                        color="black"
-                        onClick={() => {
-                          console.log("Picture-in-picture");
-                        }}
-                      />
-                      <Button
-                        style={{ fontSize: "2.5vw" }}
-                        icon="expand arrows alternate"
-                        color="black"
-                        onClick={() => {
-                          console.log("Fullscreen video");
-                        }}
-                      />
-                      <Button
-                        style={{ fontSize: "2.5vw" }}
-                        icon="history"
-                        color="black"
-                        onClick={() => {
-                          console.log("Random video");
-                        }}
-                      />
-                    </Button.Group>
-                  </div>
-                </Segment>
-              </Grid.Column>
-              <Grid.Column width={14}>
-                <Segment basic id="NoDrag">
-                  <Body />
-                </Segment>
-              </Grid.Column>
-            </Grid.Row>
-          </Grid>
-        </Segment>
-      </Sidebar.Pusher>
-    </Sidebar.Pushable>
+                </Grid.Column>
+                <Grid.Column width={14}>
+                  <Segment basic id="NoDrag">
+                    <Body />
+                  </Segment>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Segment>
+        </Sidebar.Pusher>
+      </Sidebar.Pushable>
+    </Segment>
   );
 };
 
